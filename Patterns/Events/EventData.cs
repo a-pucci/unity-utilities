d32ba485f6d8fa7f7b80040c441652b3dd9d2d30
Alessandro Pucci <a_pucci@pm.me>
2024-05-28 22:21:44 +0200

    - Changed Events system, now Events are independent

--- Patterns/Events/EventData.cs
+++ Patterns/Events/EventData.cs
@@ -1,22 +1,19 @@
-namespace AP.Utilities.Patterns
+public class EventData
 {
-	public class EventData
-	{
-		private readonly object Data;
+	private readonly object Data;
 
-		public EventData(object data) => Data = data;
+	public EventData(object data) => Data = data;
 		
-		public T GetData<T>() => (Data is T tData) ? tData : default;
+	public T GetData<T>() => (Data is T tData) ? tData : default;
 		
-		public bool TryGetData<T>(out T outData)
-		{
-			outData = default;
+	public bool TryGetData<T>(out T outData)
+	{
+		outData = default;
 			
-			if (Data is not T tData)
-				return false;
+		if (Data is not T tData)
+			return false;
 			
-			outData = tData;
-			return true;
-		}
+		outData = tData;
+		return true;
 	}
 }
